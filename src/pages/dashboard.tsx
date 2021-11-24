c2622d69be2a35489e2d4d414f7152e9df288ffe
Von Harrison <vonharrison26@gmail.com>
2021-11-24 16:05:29 -0300

    Box

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,6 +7,10 @@ export default function  Dashboard(){
         <Flex>
             <SidBar />
             <Header />
+
+            <Box bg="tomato" w="100%" p={4} color="white">
+                This is the Box
+            </Box>
         </Flex>
     )
 }
